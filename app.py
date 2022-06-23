80ef3424cc344c8c14079984d0751c78f4d7a310
dandestacamento <dandestacamento@gmail.com>
2022-06-23 19:03:54 +1000

    add method

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Hello world")
+def add(x,y):
+    return x + y
+
+if __name__ == '__main__':
+    result = add(2,3)
+    print(f"SUM:{result}")
---
+++ test.py
@@ -0,0 +1,5 @@
+import pytest
+from app import add
+
+def test1():
+    assert add(2,3) == 5
