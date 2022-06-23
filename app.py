5ecbf96f2b5d9454d2259209d20684c4b2a27109
dandestacamento <dandestacamento@gmail.com>
2022-06-23 20:01:53 +1000

    remove flask

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from datetime import datetime
 
 app = Flask(__name__)
+now = datetime.now()
 
 def add(x,y):
     return x + y
@@ -9,9 +10,11 @@ def add(x,y):
 @app.route('/')
 def home():
     result = add(2, 3)
-    now = datetime.now()
+
     return f"SUM:{result} timenow: {now}"
 
 if __name__ == '__main__':
-    app.run(port=8082)
+    # app.run(port=8082)
+    result = add(2, 3)
+    print(f"SUM:{result} timenow: {now}")
 
